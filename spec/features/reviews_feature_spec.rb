6a5f28f7993bd8bcc62476a41175c52ac04b3651
Luke Clewlow <luke.clewlow@gmail.com>
2015-02-17 15:24:12 +0000

    when you delete a restaurant, all reviews are also deleted

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 feature 'reviewing' do
 	
-	before { Restaurant.create name: 'KFC'}
+	before {Restaurant.create name: 'KFC'}
+	before {Restaurant.create name: 'Burger King'}
 
 	scenario 'allows users to leave a review using a form' do
 		visit '/restaurants'
@@ -13,6 +14,18 @@ feature 'reviewing' do
 
 		expect(current_path).to eq '/restaurants'
 		expect(page).to have_content('so so')
+	end
+
+	scenario 'deleting restaurant deletes all reviews' do
+		visit '/restaurants'
+		click_link 'Review KFC'
+		fill_in "Thoughts", with: "so so"
+		select '3', from: 'Rating'
+		click_button 'Leave Review'
+		click_link 'Delete KFC'
+
+		expect(page).not_to have_content 'KFC'
+		expect(page).to have_content 'No reviews.'
 	end
 
 end
