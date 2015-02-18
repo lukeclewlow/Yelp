5972b67b77b1ccb71a6a4a8bc8a243dbc24b0f4f
Luke Clewlow <luke.clewlow@gmail.com>
2015-02-18 19:04:23 +0000

    database issues, all sorted. game over

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,25 @@
 Rails.application.routes.draw do
 
+  root to: 'restaurants#index'
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
   resources :restaurants do
     resources :reviews
-  end
 
-  # devise_scope :user do
+  #   devise_scope :user do
   #   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   # end
+
+  end
+
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root to: 'restaurants#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
